b112b7c82f4943b4413cd42f9fe3c93ae0c072b1
sabrisaep <sabrisaep@gmail.com>
2026-06-06 10:12:12 +0800

    cubaan tambah fungsi Developer. baiki kod shell_exec()

--- equarter-v2/app/Controllers/Developer.php
+++ equarter-v2/app/Controllers/Developer.php
@@ -11,11 +11,11 @@ class Developer extends BaseController
         $output = '';
 
         if (isset($_POST['git_pull'])) {
-            $output = shell_exec('git pull 2>&1');
+            $output = \shell_exec('git pull 2>&1');
         }
 
         if (isset($_POST['migrate'])) {
-            $output = shell_exec('php spark migrate 2>&1');
+            $output = \shell_exec('php spark migrate 2>&1');
         }
 
         return view('developer/index', ['output' => $output]);
